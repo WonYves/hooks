05a292588db9e8e7ea782bdde548d3b3e3bd346e
WonYves <1053137796@qq.com>
2022-11-19 19:19:53 +0800

    1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+
+    const [num, setnum] = useState(1)
+
+    const getdata = (data) => {
+        console.log("接收子组件传来的", data);
+    }
+    return(
+        <div>
+            我是父 {num}
+            <button onClick={() => setnum(num + 1)}>add</button>
+            <Son name='kerwin' num={num} checkdata={getdata}></Son>
+        </div>
+    )
+}
+
+const Son = (props) => {
+    
+    const handleTobaby = () => {
+        props.checkdata('kerwin')
+    }
+
+    return (
+        <div>
+            我是{props.name} {props.num}
+            <button onClick={()=> handleTobaby()}>111</button>
+        </div>
+    )
+}
+
+export default App
